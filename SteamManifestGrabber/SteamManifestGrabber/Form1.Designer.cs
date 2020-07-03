adf36fd46651c612155f677e95a818c05c327b87
Kevin Henzel <khenzel@personableinsurance.com>
2020-07-03 09:15:00 -0700

    Initial Checkin
    Tested working v1.0

---
+++ SteamManifestGrabber/SteamManifestGrabber/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SteamManifestGrabber
+{
+    partial class SteamManifestGrabber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Steam Library Path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(292, 133);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(440, 35);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(292, 256);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(440, 35);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(21, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(252, 75);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Select Destination Steam Library Path";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(425, 342);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(307, 96);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Process";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // SteamManifestGrabber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SteamManifestGrabber";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Steam Manifest Grabber";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SteamManifestGrabber/SteamManifestGrabber/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SteamManifestGrabber
+{
+    public partial class SteamManifestGrabber : Form
+    {
+        public SteamManifestGrabber()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChooseFolder();
+        }
+
+
+        public void ChooseFolder()
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChooseFolder2();
+        }
+
+        public void ChooseFolder2()
+        {
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBox1.Text) && string.IsNullOrWhiteSpace(textBox2.Text))
+                {
+                    MessageBox.Show("Error. Both paths must be specified.", "Path not specified error");
+                    return;
+                }
+
+                var sourcePath = textBox1.Text;
+                var destinationPath = textBox2.Text;
+
+                var gameFolder = Path.Combine(destinationPath, @"steamapps\common");
+                var manifestFolder = Path.Combine(sourcePath, "steamapps");
+                var destinationDirectories = Directory.GetDirectories(gameFolder);
+
+                string[] myFiles = Directory.GetFiles(manifestFolder);
+
+                foreach (var d in destinationDirectories)
+                {
+                    foreach (var f in myFiles)
+                    {
+                        if (File.ReadLines(f).Any(line => line.Contains("\"installdir\"		\"" + Path.GetFileName(d) + "\"")))
+                        {
+                            File.Copy(f, Path.Combine(destinationPath, Path.GetFileName(f)), true);
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Exception: {exception}", "Exception encountered");
+            }
+        }
+    }
+}
